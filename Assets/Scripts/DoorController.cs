cc8f18d96cbd50bea3fd6055d2613e4e36e0818c
Daniëlle <moontowhee@gmail.com>
2020-06-06 01:27:24 +0200

    Update script

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject Door;
+    public bool doorIsOpening;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (doorIsOpening == true)
+        {
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+            //if the bool is true it opens the door
+        }
+        if (Door.transform.position.y > 15f)
+        {
+            doorIsOpening = false;
+        }
+
+        void OnMouseDown()
+        {
+            doorIsOpening = true;
+        }
+
+    }
+}
