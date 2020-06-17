47b945bc35788252d133b2252f64bf647702d5f2
Daniëlle <moontowhee@gmail.com>
2020-06-17 10:31:35 +0200

    Update final

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    void OnTriggerEnter(Collider col)
+    {
+        door.transform.position += new Vector3(3, 0, 0);
+    }
+}
---
+++ Assets/Scripts/TrapTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    void OnTriggerEnter(Collider col)
+    {
+        door.transform.position += new Vector3(0, 2, 0);
+    }
+}
